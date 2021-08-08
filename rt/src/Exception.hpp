8133fc233e301b859d484885bb54ae2ddaf981a2
Tom Conder <tom@itstom.co>
2021-08-08 16:26:24 -0500

    misc sonar fixes for main

--- rt/src/Exception.hpp
+++ rt/src/Exception.hpp
@@ -1,3 +1,9 @@
 #pragma once
 #include <stdexcept>
-typedef std::logic_error Exception;
+//typedef std::logic_error Exception;
+#include "String.hpp"
+
+class Exception : public std::logic_error {
+public:
+    explicit Exception(const String& arg);
+};
